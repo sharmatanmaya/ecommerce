5786965f3c5a37e6353928e8e869305eee1f17f7
sharma.tanmaya <sharma.tanmaya@gmail.com>
2023-10-08 15:35:48 -0400

    initial commit

---
+++ src/main/kotlin/com/example/eCommerce/ECommerceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.eCommerce
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ECommerceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ECommerceApplication>(*args)
+}
